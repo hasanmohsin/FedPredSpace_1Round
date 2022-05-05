2a669d33c3b73871217719459a587fc2f5b8fa48
mhasan <m67hasan@uwaterloo.ca>
2022-05-05 01:36:36 -0400

    Moved all running code to run_exp, all train functions in train-nets

--- run_exp.py
+++ run_exp.py
@@ -5,14 +5,37 @@ import argparse
 import os
 
 import matplotlib
-matplotlib.use('Agg')
 
-device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
+import datasets
+import models
+import train_nets
+import utils
+
+#device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
 
 def main(args):
+
+    ####################     
+    lr = 1e-3
+    batch_size = 100
+    num_epochs = 2
+    ####################
+
+    use_cuda = torch.cuda.is_available()
+
+    device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
+
+    print("CUDA available?: ", torch.cuda.is_available())
+    print("Device used: ", device)
+
+    trainloader, valloader  = datasets.get_mnist(use_cuda, batch_size)
+
+    net = models.LinearNet(inp_dim=28*28, num_hidden = 100, out_dim=10)
     
-    print(args)
+    net = train_nets.sgd_train(net, lr, num_epochs, trainloader)
     
+    acc = utils.classify_acc(net, valloader)
+
     return 
 
 if __name__ == '__main__':
@@ -20,16 +43,18 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=12)
     
     #dataset stuff
-    parser.add_argument('--batch_size', type=int, default = 50)
-    parser.add_argument('--train_percent', type=float, default=0.8)
+    parser.add_argument('--batch_size', type=int, default = 100)
 
-    parser.add_argument('--lr', type=float, default=1e-1)
+    parser.add_argument('--lr', type=float, default=1e-3)
 
-    parser.add_argument('--n_iters', type=int, default = 100)
+    parser.add_argument('--num_epochs', type=int, default = 5)
 
     parser.add_argument('--save_dir', type=str, default = "./plots/")
 
+    #for later - setting up train/test split
     parser.add_argument('--ntrain', type=int, default=500)
+    parser.add_argument('--train_percent', type=float, default=0.8)
+
 
     args = parser.parse_args()
     
--- train_nets.py
+++ train_nets.py
@@ -1,72 +1,36 @@
 import torch
 import numpy as np
 
-import matplotlib.pyplot as plt
+def sgd_train(net, lr, num_epochs, trainloader):
+    optimizer = torch.optim.SGD(net.parameters(), lr = lr)
 
-import matplotlib
-#matplotlib.use('Agg')
+    net = net.train()
 
-import datasets
-import models
+    criterion = torch.nn.CrossEntropyLoss()
 
-##########################
+    for i in range(num_epochs):
+        epoch_loss = 0.0
+        count = 0
+        for x, y in trainloader:
+            
+            optimizer.zero_grad()
+            pred_logits = net(x)
+            loss = criterion(pred_logits, y)
+            loss.backward()
+            optimizer.step()
 
-lr = 1e-3
-batch_size = 100
-num_epochs = 10
-##########################
+            epoch_loss += loss.item()
 
+            count+=1
 
-use_cuda = torch.cuda.is_available()
-
-device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
-
-print("CUDA available?: ", torch.cuda.is_available())
-print("Device used: ", device)
-
-trainloader, valloader  = datasets.get_mnist(use_cuda, batch_size)
-
-net = models.LinearNet(inp_dim=28*28, num_hidden = 100, out_dim=10)
-#net = CNN()
-optimizer = torch.optim.SGD(net.parameters(), lr = lr)
-
-net = net.train()
-
-criterion = torch.nn.CrossEntropyLoss()
-
-for i in range(num_epochs):
-    epoch_loss = 0.0
-    count = 0
-    for x, y in trainloader:
         
-        optimizer.zero_grad()
-        pred_logits = net(x)
-        loss = criterion(pred_logits, y)
-        loss.backward()
-        optimizer.step()
+        
+        print("Epoch: ", i+1, "Loss: ", epoch_loss)
+    print("Training Done!")
+    return net
 
-        epoch_loss += loss.item()
 
-        count+=1
 
-    
-       
-    print("Epoch: ", i+1, "Loss: ", epoch_loss)
-print("Training Done!")
-
-# test eval
-total = 0
-correct = 0
-
-net = net.eval()
-for x,y in valloader:
-    pred_logit = net(x)
-    _, pred = torch.max(pred_logit, 1)    
-
-    total += y.size(0)
-    correct += (pred == y).sum().item()
-
-print("Accuracy on test set: ", 100*correct/total)
 #def sgld_run(net, num_samples, burn_in_epochs, mix_epochs, num_samples, thin_epochs, lr):
 
     #params:
