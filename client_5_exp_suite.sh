cd0ca31777ba5c83569667d6ced1e56e1b8f9910
mhasan <m67hasan@uwaterloo.ca>
2022-05-13 08:06:42 -0400

    CNN training scripts

--- client_5_exp_suite.sh
+++ client_5_exp_suite.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./client_5_exps.sh mnist > mnist_log.txt
-#./client_5_exp.sh cifar10 > cifar10_log.txt
-#./client_5_exp.sh cifar100 > cifar100_log.txt
-./client_5_exps.sh emnist > emnist_log.txt
-./client_5_exps.sh f_mnist > f_mnist_log.txt
+#./client_5_exps.sh mnist > mnist_log.txt
+./client_5_exps.sh cifar10 > cifar10_log.txt
+#./client_5_exps.sh cifar100 > cifar100_log.txt
+#./client_5_exps.sh emnist > emnist_log.txt
+#./client_5_exps.sh f_mnist > f_mnist_log.txt
 #./client_5_exps.sh airquality > airquality_log.txt
 #./client_5_exps.sh bike > bike_log.txt
 #./client_5_exps.sh forest_fire > forest_fire_log.txt
--- client_5_exps.sh
+++ client_5_exps.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 printf "\nRunning F MCMC Experiment: \n"
-python run_exp.py --mode f_mcmc --dataset $1 --epoch_per_client 25 --lr 1e-1 --num_round 1
+python run_exp.py --mode f_mcmc --dataset $1 --epoch_per_client 50 --lr 5e-1 --num_round 1 --net_type cnn
 
-printf "\nRunning Fed SGD (no momentum) Experiment: \n"
-python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 5 --lr 1e-2 --num_round 5 --optim_type "sgd"
+#printf "\nRunning Fed SGD (no momentum) Experiment: \n"
+#python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 10 --lr 1e-2 --num_round 5 --optim_type "sgd" --net_type cnn
 
 printf "\nRunning Fed SGDM Experiment: \n"
-python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 5 --lr 1e-2 --num_round 5 --optim_type "sgdm"
+python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 10 --lr 1e-2 --num_round 5 --optim_type "sgdm" --net_type cnn
 
 printf "\nRunning Fed SGDM Experiment 1 round: \n"
-python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 25 --lr 1e-2 --num_round 1 --optim_type "sgdm"
+python run_exp.py --mode fed_sgd --dataset $1 --epoch_per_client 50 --lr 1e-2 --num_round 1 --optim_type "sgdm" --net_type cnn
 
 printf "\nRunning FedPA Experiment: \n"
-python run_exp.py --mode fed_pa --dataset $1 --epoch_per_client 5 --lr 1e-1 --num_round 5 
+python run_exp.py --mode fed_pa --dataset $1 --epoch_per_client 10 --lr 5e-1 --num_round 5 --net_type cnn
 
 printf "\nRunning FedPA Experiment 1 round: \n"
-python run_exp.py --mode fed_pa --dataset $1 --epoch_per_client 25 --lr 1e-1 --num_round 1 
+python run_exp.py --mode fed_pa --dataset $1 --epoch_per_client 50 --lr 5e-1 --num_round 1 --net_type cnn
 
 printf "\nRunning EP MCMC Experiment \n"
-python run_exp.py --mode ep_mcmc --dataset $1 --epoch_per_client 25 --lr 1e-1 --num_round 1
+python run_exp.py --mode ep_mcmc --dataset $1 --epoch_per_client 50 --lr 5e-1 --num_round 1 --net_type cnn
