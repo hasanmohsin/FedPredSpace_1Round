6f9b90c263c6b708c7cb23f051817e3ba3e427df
mhasan <m67hasan@uwaterloo.ca>
2022-05-13 06:58:23 -0400

    Regression test scripts

---
+++ regr_client_5_exp_suite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./regr_client_5_exps.sh airquality > airquality_log.txt
+./regr_client_5_exps.sh bike > bike_log.txt
+./regr_client_5_exps.sh forest_fire > forest_fire_log.txt
+./regr_client_5_exps.sh winequality > winequality_log.txt
+./regr_client_5_exps.sh real_estate > real_estate_log.txt
+
